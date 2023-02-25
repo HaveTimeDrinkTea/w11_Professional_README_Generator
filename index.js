cd912c4838610827af6d196abc70e1fc2923de18
HaveTime.DrinkTea <havetimedrinktea@gmail.com>
2023-02-25 17:19:15 +0000

    basic code structure set up for inquirer.prompt

--- index.js
+++ index.js
@@ -1,21 +1,82 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+// const fs = require("fs");
+// const path = require('path');
+// const inquirer = require("inquirer");
+
+
+//-- initialized  repository with a `package.json` file by running `npm init -y`.
+//-- installed inquirer and added it to list of dependencies by running `npm i inquirer --save`.
+//--PW Inquirer v9 and higher are native esm modules, this mean you cannot use the commonjs syntax require('inquirer') anymore. 
+
+
+
+// get fs
+import * as fs from 'fs';
+
+// get path
+import * as path from 'path';
+
+import generateMarkdown from './utils/generateMarkdown.js';
+
+console.log("What is generateMarkdown:", generateMarkdown);
+
+import inquirer from 'inquirer';
+
+// const generateMarkdown = require("./utils/generateMarkdown");
+
+// const util = require('util');
+
+import * as util from 'util';
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
 
 // array of questions for user
-const questions = [
-
+const questionsArr = [
+   {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'userName',
+   }, 
+   {
+      type: 'input',
+      message: 'What is your gender?',
+      name: 'userGender',
+   }, 
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+console.log("questionsArr:", questionsArr);
 
-// function to initialize program
-function init() {
+const promptUser = () => {
+   return inquirer.prompt(questionsArr)
+};
 
-}
+// const generateMarkdown= (userResponses) =>
+//    `# blah blah
+//    and second lien
+//    and third line
+//    bye! My name is ${userResponses.userName}`
 
-// function call to initialize program
+
+// Bonus using async/await and try/catch
+const init = async () => {
+   console.log('hi');
+   try {
+      const userResponses = await promptUser();
+      console.log("inside ini:", generateMarkdown);
+
+      console.log("userResponses",userResponses);
+      
+      const readMeFile = generateMarkdown(userResponses);
+      
+      await writeFileAsync('./output/README1.md', readMeFile);
+      
+      console.log('Successfully wrote to readme3.md');
+   } catch (err) {
+      console.log(err);
+   }
+};
+
 init();
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,21 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// function generateMarkdown(data) {
+//   return `# ${data.title}
 
-`;
+// `;
+// }
+// module.exports = generateMarkdown;
+
+export default function generateMarkdown(userResponses) {
+  const readMeText = 
+  `# blah blah
+  and second lien
+  and third line
+  bye! My name is ${userResponses.userName}
+  and I am a ${userResponses.userGender}
+  `;
+  return readMeText;
 }
 
-module.exports = generateMarkdown;
+// export { generateMarkdown };
+
