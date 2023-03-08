1f07f5c7e1a98914ceaebd9dac0b07729e317b51
HaveTime.DrinkTea <havetimedrinktea@gmail.com>
2023-03-08 10:41:57 +0000

    added current readme file name (with version num) in the welcome message.

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ let fileName = `README` + fileNum + `.md`;
 
 let welcomeMsg = `\n\n\n✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷ \n\n`
    + `                 👯‍♂️🕺🏻👯‍♀️ GOOD DAY GOOD DAY! 👯‍♀️🕺🏻👯‍♂️\n`
-   + `  Let's get WERKING and write an ELEGANZA EXTRAVAGANZA README` + fileNum + `.md! \n \n`
+   + `  Let's get WERKING and write an ELEGANZA EXTRAVAGANZA README` + (fileNum + 1) + `.md! \n \n`
    + `✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷✤∷❁∷ \n\n`
 
    
